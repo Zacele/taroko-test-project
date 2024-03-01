7509759762f814a190736ff17cff41a50444e3ca
Khoa Le <le.khoa2209@gmail.com>
2024-03-01 08:08:30 +0700

    Feat: Remove unused input field

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,8 +2,6 @@ import { ContactType } from '@/interface';
 
 function searchByName(contacts: ContactType[], query: string): ContactType[] {
   const lowerCaseQuery = query.toLowerCase();
-  console.log('contacts: ', contacts);
-
   const results = contacts.filter((contact) => {
     const fullName = `${contact.first_name} ${contact.last_name}`.toLowerCase();
     return fullName.includes(lowerCaseQuery);
